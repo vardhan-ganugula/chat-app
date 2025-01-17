5f55db78b5034f21d783e19c42eb4a61d6949f55
vardhan ganugula <gvardhan2004@gmail.com>
2025-01-18 02:51:13 +0530

    change again socket url

--- client/src/store/useAuthStrore.js
+++ client/src/store/useAuthStrore.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { isAxiosError } from "axios";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:3000/" : "/";
 export const useAuthStore = create((set, get) => ({
   onlineUsers: [],
   authUser: null,
